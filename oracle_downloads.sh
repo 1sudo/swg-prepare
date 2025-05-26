c79a68f157303d0b609c1f1d62e9afa2477e3435
Zachary Jones <zjones89@outlook.com>
2025-05-26 06:15:19 -0500

    testing

--- oracle_downloads.sh
+++ oracle_downloads.sh
@@ -4,6 +4,10 @@
 echo 'Downloading Oracle Database Utilities'
 pip3 install gdown --user
 
+if [ $? -ne 0 ]; then
+  sleep 1000
+fi
+
 #Oracle 19c Preinstall Pack
 cd ~/ora_dependencies
 gdown https://drive.google.com/uc?id=1Z-iOThbbSenPdZF1ohr-TCe17qNgVEPe
